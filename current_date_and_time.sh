6db740491cb4abe09124e463c2c3998d82e4104f
Alexey Shabunevich <a_shabunevich@wargaming.net>
2022-05-30 12:34:24 -0400

    Adding Vilnius time

--- current_date_and_time.sh
+++ current_date_and_time.sh
@@ -3,3 +3,6 @@ echo "--------------------------------------"
 echo "Script displays current date and time."
 date
 echo ""
+echo "Current date and time in Vilnius:"
+TZ='Europe/Vilnius' date
+echo ""
